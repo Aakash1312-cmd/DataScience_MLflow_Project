8be2ae39adcff45c7a80a7c023d64021dac83587
Akash Dutta <aakashgithub1312@gmail.com>
2024-01-03 22:00:28 +0530

    Done

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.DataScience_MLFlow_Project import logger
+
+
+logger.info("Welcome to custom logging")
---
+++ src/DataScience_MLFlow_Project/utils/common.py
@@ -0,0 +1,10 @@
+import os
+from exceptions import ValueError
+import yaml
+from DataScience_MLFlow_Project import logger
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
